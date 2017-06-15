e9635dee83a425b7424a5387434dfe883b2684b0
daehyun <neat507@codemind.co.kr>
2017-06-15 21:14:43 +0900

    찾기 창 메시지 Not Found로 변경

--- FastColoredTextBox/FindForm.cs
+++ FastColoredTextBox/FindForm.cs
@@ -66,7 +66,7 @@ namespace FastColoredTextBoxNS
                     FindNext(pattern);
                     return;
                 }
-                MessageBox.Show("Not found");
+                MessageBox.Show("찾을 수 없습니다.");
             }
             catch (Exception ex)
             {
--- FastColoredTextBox/SyntaxHighlighter.cs
+++ FastColoredTextBox/SyntaxHighlighter.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Text;
 
 namespace FastColoredTextBoxNS
 {
@@ -15,9 +16,9 @@ namespace FastColoredTextBoxNS
         public readonly Style BlueBoldStyle = new TextStyle(Brushes.Blue, null, FontStyle.Bold);
         public readonly Style BlueStyle = new TextStyle(Brushes.Blue, null, FontStyle.Regular);
         public readonly Style BoldStyle = new TextStyle(null, null, FontStyle.Bold | FontStyle.Underline);
-        public readonly Style BrownStyle = new TextStyle(Brushes.Brown, null, FontStyle.Italic);
+        public readonly Style BrownStyle = new TextStyle(Brushes.Brown, null, FontStyle.Regular);
         public readonly Style GrayStyle = new TextStyle(Brushes.Gray, null, FontStyle.Regular);
-        public readonly Style GreenStyle = new TextStyle(Brushes.Green, null, FontStyle.Italic);
+        public readonly Style GreenStyle = new TextStyle(Brushes.Green, null, FontStyle.Regular);
         public readonly Style MagentaStyle = new TextStyle(Brushes.Magenta, null, FontStyle.Regular);
         public readonly Style MaroonStyle = new TextStyle(Brushes.Maroon, null, FontStyle.Regular);
         public readonly Style RedStyle = new TextStyle(Brushes.Red, null, FontStyle.Regular);
